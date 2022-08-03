3bacd30db5908746afa3898ed6b1f277d83a09bd
st-vincent1 <sebastian.tate.vincent@gmail.com>
2022-08-03 14:12:29 +0100

    Updated scripts

--- examples/cue_sandbox/en-pl/src/cue_evaluate.sh
+++ examples/cue_sandbox/en-pl/src/cue_evaluate.sh
@@ -3,18 +3,25 @@
 set -e
 ROOT=examples/cue_sandbox/en-pl
 DATA=cue.en.pl
-MODEL=cue.en.pl
 
 SPM_MODEL=$ROOT/spm.bpe.model
 TMP=examples/cue_sandbox/en-pl/tmp
 mkdir -p $TMP
 
-CKPT=${1:-'checkpoint_best'}
-#
-## SPM encode
+BLOCK_CXT_ENCODER=$1
+SUFFIX=$2
+MODEL=cue.en.pl${SUFFIX}
+CKPT=${3:-'checkpoint_best'}
+mkdir -p logs
+
+if [ ${BLOCK_CXT_ENCODER} == true ]; then
+  EXTRA_ARGS=(--context-just-embed)
+fi
+
+# SPM encode
 #cat test.en-de.en | python scripts/spm_encode.py \
 #  --model ${SPM_MODEL} > ${TMP}/test.en-de.en.bpe
-#
+
 #cat ${TMP}/test.en-de.en.bpe | fairseq-interactive data-bin/${DATA}/ \
 #  --task cue_translation --source-lang en --target-lang de \
 #  --path checkpoints/cue/checkpoint_best.pt \
@@ -28,14 +35,14 @@ CKPT=${1:-'checkpoint_best'}
 #
 #rm -r ${TMP}
 
-#fairseq-generate data-bin/${DATA} \
-#    --task cue_translation --source-lang en --target-lang pl \
-#    --path checkpoints/${MODEL}/${CKPT}.pt \
-#    --batch-size 64 \
-#    --remove-bpe=sentencepiece \
-#    --context-inclusion cxt-src-concat > ${TMP}/test.sys #2> /dev/null 
-#
-#grep ^H ${TMP}/test.sys | LC_ALL=C sort -V | cut -f3- > ${TMP}/test.hyp
+fairseq-generate data-bin/${DATA} \
+    --task cue_translation --source-lang en --target-lang pl \
+    --path checkpoints/${MODEL}/${CKPT}.pt \
+    --batch-size 1024 \
+    --remove-bpe=sentencepiece \
+    --context-inclusion cxt-src-concat ${EXTRA_ARGS} > ${TMP}/test.sys 
+
+grep ^H ${TMP}/test.sys | LC_ALL=C sort -V | cut -f3- > ${TMP}/test.hyp
 
 sacrebleu $ROOT/data/en-pl.test.pl -i ${TMP}/test.hyp
 
--- examples/cue_sandbox/en-pl/src/cue_train.sh
+++ examples/cue_sandbox/en-pl/src/cue_train.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
-CKPT=checkpoints/cue.en.pl
 SEED=1
+BLOCK_CXT_ENCODER=${1:-false}
+SUFFIX=$2
 mkdir -p logs
+CKPT=checkpoints/cue.en.pl_${SUFFIX}
+
+if [ ${BLOCK_CXT_ENCODER} = true ]; then
+  EXTRA_ARGS=(--context-just-embed)
+fi
 
 mkdir -p ${CKPT}
 CUDA_VISIBLE_DEVICES=0,1 fairseq-train data-bin/cue.en.pl/ \
@@ -12,15 +18,15 @@ CUDA_VISIBLE_DEVICES=0,1 fairseq-train data-bin/cue.en.pl/ \
     --arch cue_transformer_base \
     --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9, 0.98)' \
-    --lr 0.0002 --lr-scheduler inverse_sqrt \
+    --lr 0.0005 --lr-scheduler inverse_sqrt \
     --warmup-updates 4000 --warmup-init-lr '1e-07' \
     --label-smoothing 0.1 --criterion label_smoothed_cross_entropy \
     --dropout 0.3 --weight-decay 0.0001 \
     --save-interval-updates 10000 \
     --no-epoch-checkpoints \
     --save-dir ${CKPT} \
-    --max-tokens 2000 \
+    --max-tokens 20000 \
     --memory-efficient-fp16 \
     --tensorboard-logdir logs \
     --seed ${SEED} \
-    --context-inclusion add-encoder-outputs \
+    --context-inclusion cxt-src-concat ${EXTRA_ARGS[@]}
